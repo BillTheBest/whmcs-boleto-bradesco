de978eda4d8b41a467f311c8a512ccb9cdbfef3e
Weverton Velludo <wv@brasilnetwork.com.br>
2016-12-06 14:00:13 -0200

    Update boleto.php

--- modules/gateways/boletobradesco/boleto.php
+++ modules/gateways/boletobradesco/boleto.php
@@ -52,10 +52,10 @@ if (is_numeric($invoiceid) && $invoiceid > 0) {
 	$month = substr($duedate,5,2);
 	$day = substr($duedate,8,2);
 
-	$taxa_boleto 						= $GATEWAY["taxa"];
-	$valor_cobrado 						= $total;
-	$valor_cobrado 						= str_replace(",", ".",$valor_cobrado);
-	$valor_boleto						= number_format($valor_cobrado+$taxa_boleto, 2, ',', '');
+	$taxa_boleto 				= $GATEWAY["taxa"];
+	$valor_cobrado 				= $total;
+	$valor_cobrado 				= str_replace(",", ".",$valor_cobrado);
+	$valor_boleto				= number_format($valor_cobrado+$taxa_boleto, 2, ',', '');
 
 	$dadosboleto["nosso_numero"] 		= $invoiceid;  // Nosso numero - REGRA: Máximo de 8 caracteres!
 	$dadosboleto["numero_documento"] 	= $invoiceid;	// Num do pedido ou nosso numero
@@ -65,26 +65,26 @@ if (is_numeric($invoiceid) && $invoiceid > 0) {
 	$dadosboleto["valor_boleto"] 		= number_format($valor_cobrado+$taxa_boleto, 2, ',', ''); 	// Valor do Boleto - REGRA: Com vírgula e sempre com duas casas depois da virgula
 
 	// DADOS OPCIONAIS DE ACORDO COM O BANCO OU CLIENTE
-	$dadosboleto["quantidade"] 			= "";
+	$dadosboleto["quantidade"] 		= "";
 	$dadosboleto["valor_unitario"] 		= "";
-	$dadosboleto["aceite"] 				= "N";		
-	$dadosboleto["especie"] 			= "R$";
+	$dadosboleto["aceite"] 			= "N";		
+	$dadosboleto["especie"] 		= "R$";
 	$dadosboleto["especie_doc"] 		= "DM";
-	$dadosboleto["conta"] 				= $GATEWAY["conta_cedente"];	// Num da conta, sem digito
-	$dadosboleto["conta_dv"] 			= $GATEWAY["conta_cedente_dv"]; 	// Digito do Num da conta
-	$dadosboleto["agencia"]				= substr("0000" . $GATEWAY["agencia"],-4);
-	$dadosboleto["agencia_dv"]			= $GATEWAY["agenciadv"];
-	$dadosboleto["carteira"] 			= $GATEWAY["convenio"];  // Código da Carteira: pode ser 175, 174, 104, 109, 178, ou 157
+	$dadosboleto["conta"] 			= $GATEWAY["conta_cedente"];	// Num da conta, sem digito
+	$dadosboleto["conta_dv"] 		= $GATEWAY["conta_cedente_dv"]; 	// Digito do Num da conta
+	$dadosboleto["agencia"]			= substr("0000" . $GATEWAY["agencia"],-4);
+	$dadosboleto["agencia_dv"]		= $GATEWAY["agenciadv"];
+	$dadosboleto["carteira"] 		= $GATEWAY["convenio"];  // Código da Carteira: pode ser 175, 174, 104, 109, 178, ou 157
 	$dadosboleto["identificacao"] 		= $GATEWAY["identificacao"];
-	$dadosboleto["cpf_cnpj"] 			= $GATEWAY["cpfcnpj"];
-	$dadosboleto["endereco"] 			= $GATEWAY["endereco"];
-	$dadosboleto["cidade_uf"] 			= $GATEWAY["cidade_uf"];
-	$dadosboleto["cedente"] 			= $GATEWAY["cedente"];
+	$dadosboleto["cpf_cnpj"] 		= $GATEWAY["cpfcnpj"];
+	$dadosboleto["endereco"] 		= $GATEWAY["endereco"];
+	$dadosboleto["cidade_uf"] 		= $GATEWAY["cidade_uf"];
+	$dadosboleto["cedente"] 		= $GATEWAY["cedente"];
 
 	// DADOS DO SEU CLIENTE
-	$dadosboleto["sacado"] 				= $clientsdetails["firstname"]." ".$clientsdetails["lastname"];
-	$dadosboleto["endereco1"] 			= $clientsdetails["address1"];
-	$dadosboleto["endereco2"] 			= $clientsdetails["city"].", ".$clientsdetails["state"].", ".$clientsdetails["postcode"]; 
+	$dadosboleto["sacado"] 			= $clientsdetails["firstname"]." ".$clientsdetails["lastname"];
+	$dadosboleto["endereco1"] 		= $clientsdetails["address1"];
+	$dadosboleto["endereco2"] 		= $clientsdetails["city"].", ".$clientsdetails["state"].", ".$clientsdetails["postcode"]; 
 
 	// INFORMACOES PARA O CLIENTE
 	$dadosboleto["demonstrativo1"] 		= $dadosboleto["identificacao"];
@@ -94,7 +94,7 @@ if (is_numeric($invoiceid) && $invoiceid > 0) {
 	$dadosboleto["instrucoes2"] 		= $GATEWAY["instrucoes2"];
 	$dadosboleto["instrucoes3"] 		= $GATEWAY["instrucoes3"];
 	$dadosboleto["instrucoes4"] 		= $GATEWAY["instrucoes4"];
-	$dadosboleto["urllogo"] 			= $GATEWAY["logo"];
+	$dadosboleto["urllogo"] 		= $GATEWAY["logo"];
 	
 	$boleto = new boleto_bradesco();
 	$link = $boleto->Link($dadosboleto);
